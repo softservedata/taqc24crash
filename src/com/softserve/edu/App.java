76bda4f6902eaa4124d4ba3753efad66169940f5
bebebe <hahaha@gmail.com>
2024-03-22 19:57:28 +0200

    Simple Java Project Template

---
+++ src/com/softserve/edu/App.java
@@ -0,0 +1,11 @@
+package com.softserve.edu;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("dev branch");
+    }
+}
