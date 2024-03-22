863019a9feea5c35962fc4db2a35580097c78f7e
bebebe <hahaha@gmail.com>
2024-03-22 20:01:51 +0200

    Message Updated

--- src/com/softserve/edu/App.java
+++ src/com/softserve/edu/App.java
@@ -6,6 +6,6 @@ package com.softserve.edu;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("dev branch");
+        System.out.println("dev_branch");
     }
 }
